b5c04b2d56fc24b524cebfb61dadf79c100c51a8
xanqus <sbk98031816@gmail.com>
2024-10-27 10:22:28 +0900

    init

---
+++ src/main/java/org/zeroskill/interviewassignments/InterviewAssignmentsApplication.java
@@ -0,0 +1,13 @@
+package org.zeroskill.interviewassignments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewAssignmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewAssignmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zeroskill/interviewassignments/InterviewAssignmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zeroskill.interviewassignments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewAssignmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
